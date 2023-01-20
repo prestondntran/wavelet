5220d61fce8b9a842266036cdc158478c7cf5487
prestondntran <prestondnt@gmail.com>
2023-01-19 21:23:10 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class Handler implements URLHandler {
+class SearchHandler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> strings = new ArrayList<String>();
@@ -47,6 +47,6 @@ class SearchEngine {
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new SearchHandler());
     }
 }
